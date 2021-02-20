67877f398db470bff1d74ada68c17dacd56aa1b4
VetrovDmitry007 <13im@mail.ru>
2021-02-20 16:06:50 +0300

    1. Добавлене вход по паролю с поддержной сессии 1. Добавлен "Выход" из сессии

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import os
 from LibMetod import *
 
 app = Flask(__name__,  static_folder='static')
 app.debug = True
+app.config['SECRET_KEY'] = '38899ebc4e1575cc5199d9268611741bb7569903'
 # отключить кэш
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
@@ -12,10 +13,21 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 def index():
     return render_template('authorize.html')
 
+@app.route('/exit')
+def session_exit():
+    session.pop('psw')
+    return render_template('authorize.html')
+
 @app.route('/login', methods=['GET', 'POST'])
 def authoriz():
-    # if request.method == 'POST':
-    return render_template('find.html')
+    if request.method == 'POST':
+        session['psw'] = request.form['password']
+    if ('psw' in session) and (session["psw"].strip() == '1'):
+        return render_template('find.html')
+    else:
+        return render_template('authorize.html')
+
+
 
 @app.route('/find', methods=['POST'])
 def find():
@@ -24,12 +36,21 @@ def find():
         # print(type(request.form))
         # ls_book = parsFind(request.form)
         ls_book = crSpisBook(10)
-        return render_template('tabResult.html', ls_book = ls_book)
+        if ('psw' in session) and (session["psw"].strip() == '1'):
+            return render_template('tabResult.html', ls_book = ls_book)
+        else:
+            return render_template('authorize.html')
+
+
 
 @app.route('/book/<id>')
 def infoBook(id):
     # return 'Book id: '+ str(id)
-    return render_template('infoBook.html')
+    if ('psw' in session) and (session["psw"].strip() == '1'):
+        return render_template('infoBook.html')
+    else:
+        return render_template('authorize.html')
+
 
 
 if __name__ == '__main__':
