76b78509412cfdec654457bc35aad4a0757758e6
VetrovDmitry007 <13im@mail.ru>
2021-03-11 16:12:36 +0300

    Замена форм HTML на формы Flask

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, session, send_file, request
 from App.LibMetod import *
 from App import ec_cfg
+from App.Forms import LoginForm, FindForm
 
 app = Flask(__name__, static_folder='static')
 app.debug = ec_cfg.debugFlask
@@ -12,32 +13,52 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route('/MarcWeb/Work.asp')
 @app.route('/')
 def index():
-    return render_template('authorize.html')
+    """
+    Возвращает форму авторизации пользователя
+    :return: Форма авторизации
+    """
+    login_form = LoginForm()
+    return render_template('authorize.html', form=login_form)
 
 
 @app.route('/MarcWeb/exit')
 @app.route('/exit')
 def session_exit():
+    login_form = LoginForm()
     session.pop('psw')
     session.pop('login')
-    return render_template('authorize.html')
+    # login_form.username.data = ''
+    # login_form.password.gettext('')
+    return render_template('authorize.html', form=login_form)
 
 
 @app.route('/MarcWeb/login', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
 def authoriz():
     """
-    Форма авторизации ползователя
+    После авторизации возвращает форму поиска
     :return: Форма поиска
     """
-    if request.method == 'POST':
-        session['psw'] = request.form['password']
-        session['login'] = request.form['login']
-    if ('psw' in session) and (session["psw"].strip() == ec_cfg.pswMarc) and (
-            session["login"].strip() == ec_cfg.loginMarc):
-        return render_template('find.html')
-    else:
-        return render_template('authorize.html')
+    find_form = FindForm()
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        session['login'] = login_form.username.data
+        session['psw'] = login_form.password.data
+        if ('psw' in session) and (session["psw"].strip() == ec_cfg.pswMarc) and (
+                session["login"].strip() == ec_cfg.loginMarc):
+            return render_template('find.html', form=find_form)
+    return render_template('authorize.html', form=login_form)
+
+
+@app.route('/MarcWeb/f_find', methods=['GET', 'POST'])
+@app.route('/f_find', methods=['GET', 'POST'])
+def getFrmFind():
+    """
+    Возвращает форму поиска
+    :return: Форма поиска
+    """
+    find_form = FindForm()
+    return render_template('find.html', form=find_form)
 
 
 @app.route('/MarcWeb/find', methods=['POST'])
@@ -47,17 +68,16 @@ def find():
     Возвращает список найденной литературы
     :return: Список словарей
     """
-    if request.method == 'POST':
-        ls_id = findBook(request.form)
-        marc = Class_Sql()
-        ls_book = marc.getSpisBook(ls_id)
-        if ('psw' in session) and (session["psw"].strip() == ec_cfg.pswMarc) and (
-                session["login"].strip() == ec_cfg.loginMarc):
-            fd, path = uploadPDF(ls_book)
-            StartThreadDel(fd, path)
-            return render_template('tabResult.html', ls_book=ls_book, patch_pdf=path)
-        else:
-            return render_template('authorize.html')
+    find_form = FindForm()
+    ls_id = findBook(find_form)
+    marc = Class_Sql()
+    ls_book = marc.getSpisBook(ls_id)
+    if ('psw' in session) and (session["psw"].strip() == ec_cfg.pswMarc) and (
+            session["login"].strip() == ec_cfg.loginMarc):
+        fd, path = uploadPDF(ls_book)
+        StartThreadDel(fd, path)
+        return render_template('tabResult.html', ls_book=ls_book, patch_pdf=path)
+    return render_template('authorize.html')
 
 
 @app.route('/MarcWeb/book/<id>')
@@ -72,8 +92,7 @@ def infoBook(id):
             session["login"].strip() == ec_cfg.loginMarc):
         ls_tag = getInfoBook(id)
         return render_template('infoBook.html', ls_tag=ls_tag, book_id=id)
-    else:
-        return render_template('authorize.html')
+    return render_template('authorize.html')
 
 
 @app.route('/MarcWeb/upload/<book_id>')
@@ -89,8 +108,8 @@ def upload_file(book_id):
         fd, path = tpl
         StartThreadDel(fd, path)
         return send_file(path)  # посмотреть справку по send_file()
-    else:  # если файл макрообъекта не создан
-        return render_template('find.html')
+    # если файл макрообъекта не создан
+    return render_template('find.html')
 
 
 @app.route('/MarcWeb/export/<patch_pdf>')
